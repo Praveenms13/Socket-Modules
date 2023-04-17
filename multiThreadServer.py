c9915e7de46af70c41033370900d0258850a3034
praveen <mspraveenkumar77@gmail.com>
2023-04-17 12:09:36 +0000

    AddedServers

---
+++ multiThreadServer.py
@@ -0,0 +1,33 @@
+#! /bin/python3
+
+import socket as s
+from threading import Thread
+
+HOST = "0.0.0.0"
+PORT = 3075
+
+class ClientThread(Thread):
+    def __init__(self, conn, addr):
+        Thread.__init__(self)
+        self.conn = conn
+        self.addr = addr
+        print (f"Thread {addr[0]}:{addr[1]} started....")
+    def run(self):
+        self.conn.sendall("Welcome to praveen's server\n".encode())
+        print("Waiting for data...")
+        data = self.conn.recv(1024)
+        while data:
+            print(f"From {self.addr[0]}: Received data: {data.decode()}", end="")
+            data = self.conn.recv(1024)      
+
+ 
+sck = s.socket(s.AF_INET, s.SOCK_STREAM)
+sck.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
+sck.bind((HOST, PORT))
+sck.listen()
+while True:
+    print(f"Waiting for a Connection, Listening on {HOST}:{PORT}")
+    conn, addr = sck.accept()
+    print(f"Connected by {addr[0]} and {addr[1]}")
+    ClientThread(conn, addr).start()
+  
