5ac0b8e4dd589bf4c8502188142d2afe060fc40c
Praveen <mspraveenkumar77@gmail.com>
2023-04-24 06:03:05 +0530

    update

---
+++ TCP-Chat-Application/client.php
@@ -0,0 +1,23 @@
+<?php
+
+// Define server host and port
+$host = gethostname();
+$hostIP = gethostbyname($host);
+$port = readline("Enter port number: ");
+
+// Create client socket and connect to server
+$client_socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($client_socket, $host, $port) or die("Failed to connect to $host:$port");
+
+// Continuously send messages to server
+while (true) {
+    // Get message input from user
+    echo "Enter message: ";
+    $message = trim(fgets(STDIN));
+
+    // Send message to server
+    socket_write($client_socket, $message, strlen($message));
+}
+
+// Close client socket
+socket_close($client_socket);
---
+++ TCP-Chat-Application/server.php
@@ -0,0 +1,27 @@
+<?php
+
+$host = gethostname();
+$hostIP = gethostbyname($host);
+$port = readline("Enter port number: ");
+
+$server = socket_create(AF_INET, SOCK_STREAM, 0) or die("Failed to create socket");
+socket_bind($server, $hostIP, $port) or die("Failed to bind to socket");
+socket_listen($server);
+echo "TCP Server Started\n";
+echo "Waiting for a connection, Host IP: $hostIP, Listening on Port: $port\n";
+
+while (true) {
+    $conn = socket_accept($server);
+    $addr = "";
+    socket_getpeername($conn, $addr);
+    echo "Connected by $addr[0]:$addr[1]\n";
+    socket_write($conn, "Welcome to Praveen's Server (TCP Server)");
+    echo "Waiting for Request Data...\n";
+    while ($data = socket_read($conn, 1024)) {
+        echo "Message From $addr[0]:$addr[1] => " . $data . "\n";
+        // socket_write($conn, strtoupper($data)); // Send the data back to the client
+    }
+    socket_close($conn);
+}
+
+socket_close($server);
