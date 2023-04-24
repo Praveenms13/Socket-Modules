3aa002a48176f36165024d6e2d6638394665cf4b
Praveen <mspraveenkumar77@gmail.com>
2023-04-24 06:35:26 +0530

    Update_TCP_Air

--- TCP-Chat-Application/client.py
+++ TCP-Chat-Application/client.py
@@ -1,13 +1,12 @@
 import socket
 
 # Define server host and port
-HOST = 'localhost'
-PORT = 3703
+HOST = '172.20.16.104'
+PORT = int(input('Enter port: '))
 
 # Create client socket and connect to server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
-
 # Continuously send messages to server
 while True:
     # Get message input from user
