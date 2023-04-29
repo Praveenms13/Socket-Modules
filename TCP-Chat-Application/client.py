ea18dc1122959f46d10ef5571c41b898301d4789
praveen <mspraveenkumar77@gmail.com>
2023-04-29 14:32:19 +0530

    WorkingCode

--- TCP-Chat-Application/client.py
+++ TCP-Chat-Application/client.py
@@ -1,12 +1,14 @@
 import socket
 
 # Define server host and port
-HOST = '172.20.16.104'
-PORT = int(input('Enter port: '))
+HOST = socket.gethostname()
+PORT = int(input('Enter port number: '))
 
 # Create client socket and connect to server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
+print(f"Connected to {HOST}:{PORT}")
+
 # Continuously send messages to server
 while True:
     # Get message input from user
--- TCP-Chat-Application/server.py
+++ TCP-Chat-Application/server.py
@@ -1,47 +1,27 @@
 import socket
-import threading
 
-HOST = '0.0.0.0'
-PORT = int(input('Enter port: '))
-MESSAGE_FORMAT = '{sender}: {message}'
+host = socket.gethostname()
+host_ip = socket.gethostbyname(host)
+port = int(input("Enter port number: "))
 
-def handle_client(conn, addr, clients):
-    clients.append(conn)
-    print(f'Client {addr} connected')
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host_ip, port))
+server.listen()
 
-    while True:
-        try:
-            # Receive message from client
-            message = conn.recv(1024).decode()
-            print(f'Received message from {addr}: {message}')
-
-            # Broadcast message to all connected clients
-            for client in clients:
-                if client != conn:
-                    client.sendall(MESSAGE_FORMAT.format(sender=addr, message=message).encode())
-
-            # Send response back to client
-            response = 'Message received'
-            conn.sendall(response.encode())
-
-        except:
-            # Remove client from list of connected clients
-            clients.remove(conn)
-            print(f'Client {addr} disconnected')
-            conn.close()
-            break
-
-# Create server socket and listen for incoming connections
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen()
-
-print(f'Server started on {HOST}:{PORT}')
-
-# List to store connected clients
-clients = []
+print("TCP Server Started")
+print(f"Waiting for a connection, Host IP: {host_ip}, Listening on Port: {port}")
 
 while True:
-    # Accept new connection and start new thread to handle it
-    conn, addr = server_socket.accept()
-    threading.Thread(target=handle_client, args=(conn, addr, clients)).start()
+    conn, addr = server.accept()
+    print(f"Connected by {addr[0]}:{addr[1]}")
+    conn.sendall(b"Welcome to Praveen's Server (TCP Server)")
+    print("Waiting for Request Data...")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print(f"Message From {addr[0]}:{addr[1]} => {data.decode()}")
+        # conn.sendall(data.upper())  # Send the data back to the client
+    conn.close()
+
+server.close()
