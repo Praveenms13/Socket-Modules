de21e1800bad992fbb131120edc4b716710bddb8
praveen <mspraveenkumar77@gmail.com>
2023-06-01 14:12:02 +0530

    Updated_RemoteCodeExecution_OverTCP

--- TCP-Remote-Code-Execution/tcp_client.py
+++ TCP-Remote-Code-Execution/tcp_client.py
@@ -1,21 +1,24 @@
-import socket as s
+import socket
 
-hostIP = '172.20.19.159'#s.gethostbyname(s.gethostname())
-port = int(input("Enter port number: "))
+def start_client():
+    hostIP = '172.20.19.159'  # s.gethostbyname(s.gethostname())
+    port = int(input("Enter port number: "))
 
-client = s.socket(s.AF_INET, s.SOCK_STREAM)
-client.connect((hostIP, port))  # Connect to the hostIP variable
-data = client.recv(1024)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((hostIP, port))
+    data = client.recv(1024)
 
-print(f"Welcome Message: {data.decode('utf-8')} from host: {hostIP}")
+    print(f"Welcome Message: {data.decode('utf-8')} from host: {hostIP}")
 
-while True:
-    data = input("Enter data to send (Request): ")
-    client.sendall(data.encode("utf-8"))
-    if data == "exit":
-        break
-    # else:
-    #     data = client.recv(1024)
-    #     print(f"Data from server (Response): {data.decode('utf-8')}")
+    while True:
+        data = input("Enter the Command > ")
+        client.sendall(data.encode("utf-8"))
+        if data == "exit":
+            break
+        else:
+            data = client.recv(1024)
+            print(f"{data.decode('utf-8')}")
 
-client.close()
+    client.close()
+
+start_client()
--- TCP-Remote-Code-Execution/tcp_server.py
+++ TCP-Remote-Code-Execution/tcp_server.py
@@ -1,21 +1,37 @@
-import socket as s
+import socket
+import subprocess
 
-hostIP = '0.0.0.0'#s.gethostbyname(s.gethostname())
-port = int(input("Enter port number: "))
+def run_command(command):
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    if result.returncode == 0:
+        return result.stdout
+    else:
+        return result.stderr
 
-server = s.socket(s.AF_INET, s.SOCK_STREAM)
-server.bind((hostIP, port))
-server.listen()
-print("TCP Server Started")
-print(f"Waiting for a connection, Host IP: {hostIP}, Listening on Port: {port}")
+def start_server():
+    hostIP = "0.0.0.0"
+    port = int(input("Enter port number: "))
 
-while True:
-    conn, addr = server.accept()
-    print(f"Connected by {addr[0]}:{addr[1]}")
-    conn.sendall("Welcome to Praveen's Server (TCP Server)".encode("utf-8"))
-    print("Waiting for Request Data...")
-    data = conn.recv(1024)
-    while data:
-        print(f"Message From {addr[0]}:{addr[1]} => {data.decode('utf-8')}")
-        # conn.sendall(data.upper())  # Send the data back to the client
-        data = conn.recv(1024) 
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((hostIP, port))
+    server.listen()
+    print("TCP Server Started")
+    print(f"Waiting for a connection, Host IP: {hostIP}, Listening on Port: {port}")
+
+    while True:
+        conn, addr = server.accept()
+        print(f"Connected by {addr[0]}:{addr[1]}")
+        conn.sendall("Welcome to Praveen's Server (TCP Server)".encode("utf-8"))
+        print("Waiting for Request Data...")
+        data = conn.recv(1024)
+        
+        while data:
+            print(f"Command From {addr[0]}:{addr[1]} => {data.decode('utf-8')}")
+            print("Executing the command...")
+            command = data.decode("utf-8")
+            output = run_command(command)
+            response = f"Output of the command:\n{output}".encode("utf-8")
+            conn.sendall(response)
+            data = conn.recv(1024)
+
+start_server()
