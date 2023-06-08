8b6d72448f10633bb90dcbec628c5d7f68c38907
praveen <mspraveenkumar77@gmail.com>
2023-06-08 19:33:01 +0530

    Updated

--- sample-http-python-server/sample-server.py
+++ sample-http-python-server/sample-server.py
@@ -1,95 +1,47 @@
 import socket as s
 from threading import Thread
+import mimetypes as m
 
-resp = """
-HTTP/1.1 200 OK
-Age: 230428
-Cache-Control: max-age=604800
-Content-Type: text/html; charset=UTF-8
-Date: Thu, 08 Jun 2023 09:06:44 GMT
-Etag: "3147526947+gzip"
-Expires: Thu, 15 Jun 2023 09:06:44 GMT
-Last-Modified: Thu, 17 Oct 2019 07:18:26 GMT
-Server: ECS (dcb/7FA3)
+http_resp_template = """HTTP/1.1 200 OK
+Connection: Keep-Alive
+Content-Type: {type}
+Accept-Ranges: bytes
+Content-Length: {length}
 Vary: Accept-Encoding
-X-Cache: HIT
-Content-Length: 1256
-Connection: close
+Server: Apache/2.4.41 (Ubuntu)
 
-<!doctype html>
-<html>
-<head>
-    <title>Example Domain</title>
+{body}"""
 
-    <meta charset="utf-8" />
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <style type="text/css">
-    body {
-        background-color: #f0f0f2;
-        margin: 0;
-        padding: 0;
-        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-        
-    }
-    div {
-        width: 600px;
-        margin: 5em auto;
-        padding: 2em;
-        background-color: #fdfdff;
-        border-radius: 0.5em;
-        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
-    }
-    a:link, a:visited {
-        color: #38488f;
-        text-decoration: none;
-    }
-    @media (max-width: 700px) {
-        div {
-            margin: 0 auto;
-            width: auto;
-        }
-    }
-    </style>    
-</head>
+htdocs = 'htdocs'
 
-<body>
-<div>
-    <h1>Example Domain</h1>
-    <p>This domain is for use in illustrative examples in documents. You may use this
-    domain in literature without prior coordination or asking for permission.</p>
-    <p><a href="https://www.iana.org/domains/example">More information...</a></p>
-</div>
-</body>
-</html>
-"""
+HOST = '0.0.0.0' #configure port forwarding to get internet traffic
+PORT = 3074
 
-HOST = "127.0.0.1"
-PORT = 2000
+class ASPIncomingThread(Thread):
+	def __init__(self, conn, addr):
+		Thread.__init__(self)
+		self.conn = conn
+		self.addr = addr
+		print("Thread {}:{} started...".format(addr[0], addr[1]))
+	
+	def run(self):
+		data = conn.recv(4096)
+		rl = data.decode().split('\n')[0]
+		print(rl)
+		f = rl.split(' ')[1]
+		if f == "/":
+			f = "index.html"
+		f = 'htdocs/'+f
+		with open(f, 'r') as file:
+			body = file.read()
+		self.conn.sendall(http_resp_template.format(type=m.guess_type(f)[0], length=len(body), body=body).encode())
 
-
-class sIncomingThread(Thread):
-    def __init__(self, conn, addr):
-        Thread.__init__(self)
-        self.conn = conn
-        self.addr = addr
-        print(f"Thread {addr[0]}:{addr[1]} started...")
-
-    def run(self):
-        self.conn.sendall(resp.encode("utf-8"))
-        # data = self.conn.recv(1024)
-        # while data:
-        #     print("{}: ".format(self.addr[0]) + data.decode(), end="")
-        #     data = self.conn.recv(1024)
-
-
-server = s.socket(s.AF_INET, s.SOCK_STREAM)
-server.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
-server.bind((HOST, PORT))
-server.listen()
-
+sck = s.socket(s.AF_INET, s.SOCK_STREAM)
+sck.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
+sck.bind((HOST, PORT))
+sck.listen()
 while True:
-    print("waiting for new connection...")
-    conn, addr = server.accept()
-    print(f"Connection received from {addr[0]}:{addr[1]}")
-    sIncomingThread(conn, addr).start()
+	print("Waiting for a new connection...")
+	conn, addr = sck.accept()
+	print("Connection received from {}:{}".format(addr[0], addr[1]))
+	ASPIncomingThread(conn, addr).start()
