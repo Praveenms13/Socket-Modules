3368a36dcfdac1ff84a885c35fa7dbd28e403e3b
praveen <mspraveenkumar77@gmail.com>
2023-06-10 17:30:37 +0530

    FixedBug-in-bc-server

--- bc-server/MultiThreadedMathServer.py
+++ bc-server/MultiThreadedMathServer.py
@@ -3,18 +3,26 @@ from subprocess import Popen, STDOUT, PIPE
 from threading import Thread
 
 host_ip = "0.0.0.0"
-port = 2000
+port = 20000
+con = []
 
 
 class OutputThread(Thread):
-    def __init__(self, cmd, conn):
+    def __init__(self, cmd, conn, addr):
         super().__init__()
         self.cmd = cmd
         self.conn = conn
+        self.addr = addr
 
     def run(self):
         while self.cmd.poll() is None:
-            self.conn.sendall(self.cmd.stdout.readline())
+            try:
+                self.conn.sendall(self.cmd.stdout.readline())
+            except Exception as e:
+                print(
+                    f"Connection resest for => {self.addr[0]}:{self.addr[1]}\nError => {e}"
+                )
+                con.remove(self.addr[0])
 
 
 class MathServerThread(Thread):
@@ -25,7 +33,7 @@ class MathServerThread(Thread):
 
     def run(self):
         cmdexec = Popen(["bc"], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-        out_t = OutputThread(cmdexec, self.conn)
+        out_t = OutputThread(cmdexec, self.conn, self.addr)
         out_t.start()
         while cmdexec.poll() is None:
             try:
@@ -36,15 +44,13 @@ class MathServerThread(Thread):
                 if cmd == "kill" or cmd == "exit" or cmd == "quit":
                     cmdexec.kill()
                     break
+                cmd = cmd + "\n"
+                cmdexec.stdin.write(cmd.encode())
+                cmdexec.stdin.flush()
             except Exception as e:
-                print(
-                    f"Error message from => {self.addr[0]}:{self.addr[1]}\nError => {e}"
-                )
-            cmd = cmd + "\n"
-            cmdexec.stdin.write(cmd.encode())
-            cmdexec.stdin.flush()
-
-        self.conn.close()
+                print(f"{self.addr[0]}:{self.addr[1]}\nError => {e}")
+                self.conn.close()
+                con.remove(self.addr[0])
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -52,7 +58,12 @@ server.bind((host_ip, port))
 server.listen()
 print("TCP Server Started")
 while True:
-    conn, addr = server.accept()
+    conn, addr = server.accept() 
+    con.append(addr[0])
+    if addr[0] in con:
+        print("Connection Rejected {}:{}".format(addr[0], addr[1]))
+        conn.close()
+        continue
     print("Connection received from {}:{}".format(addr[0], addr[1]))
     t = MathServerThread(conn, addr)
     t.start()
